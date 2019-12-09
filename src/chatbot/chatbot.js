ba93aece82795b9388bc44647133e3c43cac3a5d
dvasilkov <dvasilkov@kraftway.ru>
2019-12-09 12:03:41 +0300

    init

---
+++ src/chatbot/chatbot.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './chatbot.css';
+
+
+export default class ChatBot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+ 
+
+  render() {
+
+
+    return (<p>init</p>);
+  }
+}
